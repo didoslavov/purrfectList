111f28802006ea3127231258cb4006dfc7796956
didkoslawow <didkoslawow@gmail.com>
2023-04-18 16:20:03 +0300

    seting up firebase for the project

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js';
+import { getDatabase, ref, push } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js';
+
+const appSettings = {
+  databaseURL: 'https://listme-5291d-default-rtdb.europe-west1.firebasedatabase.app/',
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const productsInDB = ref(database, 'products');
+
 const addProductBtn = document.getElementById('add-btn');
 const ulElement = document.getElementById('product-list');
 const inputElement = document.getElementById('input-field');
@@ -6,6 +17,7 @@ addProductBtn.addEventListener('click', onAddProduct);
 function onAddProduct() {
   const input = inputElement.value;
   if (input != '') {
+    push(productsInDB, input);
     const li = document.createElement('li');
     li.textContent = input;
     const btn = document.createElement('button');
