a41711ef1123094704137068d7e977f7213ec7a4
didkoslawow <didkoslawow@gmail.com>
2023-04-23 14:43:49 +0300

    added animation when menu opens

--- src/app.js
+++ src/app.js
@@ -8,5 +8,6 @@ const menuIcon = document.querySelector('.menu-icon');
 const menu = document.querySelector('.menu');
 
 menuIcon.addEventListener('click', () => {
+  menuIcon.classList.toggle('active');
   menu.classList.toggle('active');
 });
