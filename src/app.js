5680556e4524b4f912880da1d9e0096c32f61762
didkoslawow <didkoslawow@gmail.com>
2023-04-29 14:57:58 +0300

    added middleware

--- src/app.js
+++ src/app.js
@@ -1,26 +1,7 @@
 // DON'T FORGET TO CHANGE SEQURITY SETTINGS IN DATABASE LATER !!!
-import page from '../../node_modules/page/page.mjs';
-import { render } from './firebase.js';
-
-render();
+import { page } from './lib.js';
+import decorateContext from './middlewares/render.js';
 
 page(decorateContext);
-page('/', homepage);
+page('/', () => console.log('Home Page'));
 page.start();
-
-const root = document.querySelector('main');
-console.log(root);
-
-const menuIcon = document.querySelector('.menu-icon');
-const menu = document.querySelector('.menu');
-
-menuIcon.addEventListener('click', () => {
-  menuIcon.classList.toggle('active');
-  menu.classList.toggle('active');
-});
-
-// TO DO - HIDE MENU WHEN CLICK ANYWARE ON THE DOCUMENT
-
-function decorateContext(ctx) {
-  ctx.render = (content) => render(content, root);
-}
---
+++ src/middlewares/render.js
@@ -0,0 +1,13 @@
+import { render } from '../lib.js';
+
+const root = document.querySelector('main');
+
+function boundRender(content) {
+  render(content, root);
+}
+
+export default function decorateContext(ctx, next) {
+  ctx.render = boundRender();
+
+  next();
+}
