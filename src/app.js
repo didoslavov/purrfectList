7d0d1fafe8d72612734710e7413e8ee52d0c0d14
didkoslawow <didkoslawow@gmail.com>
2023-04-29 23:24:45 +0300

    added register functionality

--- src/app.js
+++ src/app.js
@@ -3,9 +3,11 @@ import { page } from './lib.js';
 import decorateContext from './middlewares/render.js';
 import { homePage } from './views/home.js';
 import { loginPage } from './views/login.js';
+import { regiisterPage } from './views/register.js';
 
 page(decorateContext);
 page('/', homePage);
 page('/login', loginPage);
+page('/register', regiisterPage);
 
 page.start();
---
+++ src/views/register.js
@@ -0,0 +1,44 @@
+import { register } from '../api/data.js';
+import { html } from '../lib.js';
+
+const regiisterTemplate = (onSubmit) => html` <div class="container">
+  <form @submit=${onSubmit}>
+    <input class="input-field" type="text" name="username" placeholder="Username" />
+    <input class="input-field" type="text" name="email" placeholder="Email" />
+    <input class="input-field" type="password" name="password" placeholder="Password" />
+    <input class="input-field" type="password" name="repass" placeholder="Repeat Password" />
+    <button class="btn">Register</button>
+  </form>
+</div>`;
+
+export function regiisterPage(ctx) {
+  ctx.render(regiisterTemplate(onSubmit));
+
+  async function onSubmit(e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const username = formData.get('username');
+    const email = formData.get('email');
+    const password = formData.get('password');
+    const repass = formData.get('repass');
+
+    try {
+      if (email == '' || password == '') {
+        throw new Error('All fields are required!');
+      }
+
+      if (password != repass) {
+        throw new Error("Passwords don't match!");
+      }
+
+      //TO DO: Server side error hendling
+
+      await register(username, email, password);
+    } catch (error) {
+      alert(error.message);
+    }
+
+    ctx.page.redirect('/');
+  }
+}
