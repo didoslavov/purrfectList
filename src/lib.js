f9a7379bf72e0e518a05f882da66a6eabe72f08c
didkoslawow <didkoslawow@gmail.com>
2023-04-30 11:53:25 +0300

    added dependencies

--- src/lib.js
+++ src/lib.js
@@ -1,5 +1,5 @@
-import { html, render } from '../node_modules/lit-html/lit-html.js';
-import { until } from '../node_modules/lit-html/directives/until.js';
-import page from '../node_modules/page/page.mjs';
+import { html, render } from './lib/lit-html.js';
+import { until } from './lib/directives/until.js';
+import page from './lib/page.mjs ';
 
 export { html, render, until, page };
